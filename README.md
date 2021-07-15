c9db9657df01f022fcc2f059fa270307b5ec1114
sirius6323 <sirius6323@gmail.com>
2021-07-14 22:19:23 -0700

    Initial commit of all my files

---
+++ README.md
@@ -0,0 +1,29 @@
+# Portfolio-Website-GH
+
+- Personal portfolio to showcase current or past projects
+
+## Tech Stack
+
+- HTML
+- CSS
+- JavaScript
+
+## Dependencies
+
+- Google Fonts - Space Mono
+
+[Space Mono Font Regular 400 Bold 700](https://fonts.google.com/specimen/Space+Mono#standard-styles)
+
+- AOS Scroll Library 2.3.4
+
+[CDN Link CSS](https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css)
+
+[CDN Link JS](https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js)
+
+- typeit jQuery Animated Typing Plugin 7.0.4
+
+[CDN Link JS](https://cdnjs.cloudflare.com/ajax/libs/typeit/7.0.4/typeit.min.js)
+
+- gsap JS Library Animations 3.7.0
+
+[CDN Link JS](https://cdnjs.cloudflare.com/ajax/libs/gsap/3.7.0/gsap.min.js)
---
+++ js/index.js
@@ -0,0 +1,106 @@
+const menu = document.querySelector('.menu');
+const navOpen = document.querySelector('.hamburger');
+const navClose = document.querySelector('.close');
+const navBar = document.querySelector('.nav');
+
+const navLeft = menu.getBoundingClientRect().left;
+navOpen.addEventListener('click', () => {
+	if (navLeft < 0) {
+		menu.classList.add('show');
+		document.body.classList.add('show');
+		navBar.classList.add('show');
+	}
+});
+
+navClose.addEventListener('click', () => {
+	if (navLeft < 0) {
+		menu.classList.remove('show');
+		document.body.classList.remove('show');
+		navBar.classList.remove('show');
+	}
+});
+
+// Fixed Nav
+const navHeight = navBar.getBoundingClientRect().height;
+window.addEventListener('scroll', () => {
+	const scrollHeight = window.pageYOffset;
+	if (scrollHeight > navHeight) {
+		navBar.classList.add('fix-nav');
+	} else {
+		navBar.classList.remove('fix-nav');
+	}
+});
+
+// Scroll To
+const links = [...document.querySelectorAll('scroll-link')];
+
+links.map((link) => {
+	link.addEventListener('click', (e) => {
+		e.preventDefault();
+
+		const id = e.target.getAttribute('href').slice(1);
+		const el = document.getElementById(id);
+		let position = el.offsetTop - navHeight;
+
+		window.scrollTo({
+			top: position,
+			left: 0,
+		});
+
+		navBar.classList.remove('show');
+		menu.classList.remove('show');
+		document.body.classList.remove('show');
+	});
+});
+
+new TypeIt('#type1', {
+	speed: 120,
+	loop: true,
+	waitUntilVisible: true,
+})
+	.type('Developer', { delay: 400 })
+	.pause(500)
+	.delete(9)
+	.type('Designer', { delay: 400 })
+	.pause(500)
+	.delete(9)
+	.go();
+
+new TypeIt('#type2', {
+	speed: 120,
+	loop: true,
+	waitUntilVisible: true,
+})
+	.type('Developer', { delay: 400 })
+	.pause(500)
+	.delete(9)
+	.type('Designer', { delay: 400 })
+	.pause(500)
+	.delete(9)
+	.go();
+
+// GSAP
+gsap.from('.logo', { opacity: 0, duration: 1, delay: 0.5, y: -10 });
+gsap.from('.hamburger', { opacity: 0, duration: 1, delay: 1, x: 20 });
+gsap.from('.banner', { opacity: 0, duration: 1, delay: 1.5, x: -200 });
+gsap.from('.hero h1', { opacity: 0, duration: 1, delay: 2.5, y: -50 });
+gsap.from('.hero h3', { opacity: 0, duration: 1, delay: 2, y: -45 });
+gsap.from('.hero h4', { opacity: 0, duration: 1, delay: 3, y: -30 });
+gsap.from('.hero a', { opacity: 0, duration: 1, delay: 3.5, y: 50 });
+gsap.from('.nav-item', {
+	opacity: 0,
+	duration: 1,
+	delay: 1.2,
+	y: 30,
+	stagger: 0.2,
+});
+
+gsap.from('.icons span', {
+	opacity: 0,
+	duration: 1,
+	delay: 4,
+	y: -30,
+	stagger: 0.2,
+});
+
+AOS.init();
